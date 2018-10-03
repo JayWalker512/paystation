56b3972a12e7e02f3d620fa355b7c53744c70853
James Ryan <jryan0004@gmail.com>
2018-10-03 11:56:15 -0400

    Add main class

---
+++ src/paystation/domain/PayStationMain.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paystation.domain;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author kingJ
+ */
+public class PayStationMain {
+     
+    public static void main(String[] args) {
+        BufferedReader br = 
+                 new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println("Welcome to the PayStation");
+        System.out.println("Please select and option:");
+        System.out.println("1.Deposit Coins\n2.Display\n3.Buy Ticket\n4.Cancel\4nChange Rate Strategy");
+        
+    }
+    
+}
