c32aa12a8bee14f0114dadd67bf3c58135450feb
James Ryan <jryan0004@gmail.com>
2018-10-03 12:19:07 -0400

    Added option menu

--- src/paystation/domain/PayStationMain.java
+++ src/paystation/domain/PayStationMain.java
@@ -6,7 +6,10 @@
 package paystation.domain;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,13 +18,41 @@ import java.io.InputStreamReader;
 public class PayStationMain {
      
     public static void main(String[] args) {
+        int answer=0;
+        
         BufferedReader br = 
                  new BufferedReader(new InputStreamReader(System.in));
         
         System.out.println("Welcome to the PayStation");
-        System.out.println("Please select and option:");
-        System.out.println("1.Deposit Coins\n2.Display\n3.Buy Ticket\n4.Cancel\4nChange Rate Strategy");
-        //vghfjfjtest
+        System.out.println("Please select a number:");
+        System.out.println("1. Deposit Coins\n2. Display\n3. Buy Ticket\n4. Cancel\n5. Change Rate Strategy");
+        try {
+            answer = Integer.parseInt(br.readLine());
+            //vghfjfjtest
+        } catch (IOException | NumberFormatException ex) {
+            System.out.println("Pleasw enter a number");
+            //Logger.getLogger(PayStationMain.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        switch (answer) {
+            case 1:
+                System.out.println("");
+                break;
+            case 2:
+                System.out.println("");
+                break;
+            case 3:
+                System.out.println("");
+                break;
+            case 4:
+                System.out.println("");
+                break;
+            case 5:
+                System.out.println("");
+                break;
+            default:
+                System.out.println("GoodBye");
+                break;
+        }
         
     }
     
