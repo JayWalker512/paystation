4ee5b4dddd1201ccce26b0119ebff313313be03f
Brandon Foltz <bsfoltzguitar@gmail.com>
2018-10-09 19:00:22 -0400

    Fix ProgressiveRateStrategy

--- src/paystation/domain/ProgressiveRateStrategy.java
+++ src/paystation/domain/ProgressiveRateStrategy.java
@@ -10,22 +10,18 @@ package paystation.domain;
  * @author reric
  */
 public class ProgressiveRateStrategy implements RateStrategy {
-    
-    private int timeBoughtMinutes = 0; //60 is an hour
-    
     @Override
     public int calculateTime(int payment) {
         
-        if (timeBoughtMinutes <= 60 || payment <= 150) {
-            timeBoughtMinutes = (payment*2)/5;
+        if (payment < 150) {
+            return (payment*2)/5;
         }
-        else if ((timeBoughtMinutes > 60 && timeBoughtMinutes <= 120) || (350 > payment && payment >= 150)) {
-            timeBoughtMinutes = (payment-150)*(3/10)+60;
+        
+        if (350 > payment && payment >= 150) {
+            return (int)(((float)(payment-150)*(0.3))+60);
         }
-        else if (timeBoughtMinutes > 120 || payment >=350) {
-            timeBoughtMinutes = (payment-350)/5 + 120;
-        }
-        return timeBoughtMinutes;
+        
+        //if the first two conditions fail, we're definitely over 350 payment
+        return (payment-350)/5 + 120;
     }
-    
 }
